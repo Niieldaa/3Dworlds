fc84615afc8ccd6e1ae9bd2c1f4e1f958eb0207c
R3ALITI3S <nfjo23@student.aau.dk>
2024-11-09 23:14:38 +0100

    shoot anim

--- JamJam/Assets/Scripts/StaffMagic.cs
+++ JamJam/Assets/Scripts/StaffMagic.cs
@@ -4,44 +4,56 @@ using UnityEngine;
 
 public class StaffMagic : MonoBehaviour
 {
-
     public float damage = 10f;
-
     public float range = 100f;
 
     [SerializeField] private Camera fpsCam;
     public ParticleSystem flash;
+
+    [SerializeField] private Animator anim;
+    private bool shoot = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        // Check if Fire1 button is pressed down
+        if (Input.GetButtonDown("Fire1"))
         {
+            shoot = true;  // Set shoot to true when firing starts
+            anim.SetBool("shoot", true);  // Set the animator parameter to start the animation
             Shoot();
+        }
+
+        // Check if Fire1 button is released
+        if (Input.GetButtonUp("Fire1"))
+        {
+            shoot = false;  // Set shoot to false when firing stops
+            anim.SetBool("shoot", false);  // Reset the animation parameter
         }
     }
 
     void Shoot()
     {
+        // Play the particle effect
         flash.Play();
-        RaycastHit Hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out Hit, range))
+
+        // Perform a raycast to detect hit
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(Hit.transform.name);
+            Debug.Log(hit.transform.name);
 
-            Target target = Hit.transform.GetComponent<Target>();
-
+            Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
-                target.TakeDamage(damage);
+                target.TakeDamage(damage);  // Apply damage to the target
             }
         }
-        
-        
     }
 }
